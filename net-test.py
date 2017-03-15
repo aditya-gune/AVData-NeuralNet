5825a8c606bb18be825016158d731de42792dbd8
agune001 <ca_adi@yahoo.com>
2017-03-14 19:42:37 -0700

    BOW encoding complete

--- net-test.py
+++ net-test.py
@@ -4,6 +4,8 @@
 """
 import re
 import numpy
+from collections import Counter
+import string
 #import netfunctions
 #from keras.models import Sequential
 #from keras.layers import Dense
@@ -15,10 +17,11 @@ numpy.random.seed(seed)
 dataset = numpy.genfromtxt('TestQ.csv',delimiter=',', dtype=None)
 
 x=[]
+xwords=[]
 y=[]
 rawdata=[]
 a=[]  
-
+temp=[]
 #parse data
 for i in dataset:
     j = str(i)
@@ -45,7 +48,32 @@ numpy.random.shuffle(rawdata)   #shuffle rawdata
 #create X and Y matrices
 for i in rawdata:
     a=[]
-    a.append(i[0])
+    #a.append(i[0])
     a.append(i[1])
-    x.append(a)
+    xwords.append(a)
     y.append([i[2]])
+
+#temp =numpy.array(xwords)
+#temp = temp.tolist()
+
+str1=''
+for i in xwords:
+    i = str(i).translate(None, string.punctuation)
+    temp.append(i)
+    str1 += ' '+i
+    
+
+counts = Counter(str1.split())
+numwords = len(counts)
+dict={}
+counter = 100
+for i in counts:
+    dict[i] = counter
+    counter += 1
+
+for i in temp:
+    a = i.split()
+    b = []
+    for j in a:
+        b.append(dict[j])
+    x.append(b)
